fd4bfb5aeba7038ca5cba89eeecb1a169ec3a9d0
Vinh Pham <phamv1@unlv.nevada.edu>
2022-01-12 19:55:53 -0800

    sending index and show data

--- server.js
+++ server.js
@@ -6,11 +6,19 @@ require('dotenv').config();
 
 const PORT = process.env.PORT;
 
+// MIDDLEWARE
+app.set('views', __dirname + '/views')
+app.set('view engine', 'jsx')
+app.engine('jsx', require('express-react-views').createEngine())
+
 app.get('/', (req, res) => {
     res.send('Welcome to Awesome App about Breads');
 })
 
 // Breads
+/* If user have /bread ... in url then use the breadsController to 
+    determine the appropriate action
+*/
 const breadsController = require('./controllers/breads_controllers.js')
 
 app.use('/breads', breadsController)
